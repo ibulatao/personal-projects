18fd8e633b1c876290b2a57554ffafd7768c1abc
ibulatao <ibulatao@icloud.com>
2025-01-28 09:22:40 -0800

    Add files via upload

---
+++ variables and types.py
@@ -0,0 +1,12 @@
+# LearnPython.org Exercise 2 for Variables and Types
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# Testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
